aa7f69433eac30b8bc9a2c622be582eaf612a397
Mark Voishvillo <93478489+SegmentationFaultEnjoyer@users.noreply.github.com>
2021-12-02 19:36:47 +0200

    Add files via upload

---
+++ inc/ush.h
@@ -0,0 +1,17 @@
+#ifndef SH
+#define SH
+
+#include "../libmx/inc/libmx.h"
+#include <dirent.h>
+#include <sys/types.h>
+#include <limits.h> 
+#include <sys/acl.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/xattr.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+
+#endif
+
