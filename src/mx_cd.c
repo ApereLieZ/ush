68d7738dc32ba7140e966f1ef31ec2229702f1ee
SegmentationFaultEnjoyer <mwoyshvillo@gmail.com>
2021-12-06 21:01:35 +0200

    JOPA

--- src/mx_cd.c
+++ src/mx_cd.c
@@ -39,24 +39,24 @@ static void check_flags(char **args, bool *fl_P, bool *fl_s) {
 }
 
 void mx_cd(char **args) {
-
     // char cur_dir[1024];
     // getcwd(cur_dir, 1024);
     // setenv("PWD", cur_dir, 1);
 
     struct stat buf;
     bool fl_P = false, fl_s = false;
-    errno = 0;
     int argc = count_args(args);
     check_flags(args, &fl_P, &fl_s);
     char *path = args[argc - 1]; 
+    
     lstat(path, &buf);
+    errno = 0;
     if(!prev_path) { //init pathes
         prev_path = malloc(sizeof(char) * 1024);
         getcwd(prev_path, 1024);
         cur_path = mx_strdup(prev_path);
     }
-
+    
     if(mx_strcmp(path, "cd") == 0) { //cd without path
         chdir(getenv("HOME"));
         get_new_pathes(&prev_path, &cur_path);
@@ -75,19 +75,20 @@ void mx_cd(char **args) {
         was_in_link = false;
         return;
     }
-
+    
     char *fixed_path = mx_replace_substr(path, "~", getenv("HOME")); // ~ fix
     chdir(fixed_path); //default
-
+    
     if(S_ISLNK(buf.st_mode) && !fl_P) {
         was_in_link = true;
     }
-    get_new_pathes(&prev_path, &cur_path);
-
+    
     if(errno == ENOENT) {
         mx_printerr("cd: no such file or directory: ");
         mx_printerr(fixed_path);
         mx_printerr("\n");
     }
+    
+    get_new_pathes(&prev_path, &cur_path);
     mx_strdel(&fixed_path);
 }
