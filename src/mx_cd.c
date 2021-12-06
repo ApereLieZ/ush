d5bdeee7327edcf65257efcb0662930e5cb965f2
SegmentationFaultEnjoyer <mwoyshvillo@gmail.com>
2021-12-06 18:33:45 +0200

    "fag"

--- src/mx_cd.c
+++ src/mx_cd.c
@@ -26,29 +26,25 @@ static int count_args(char **args) {
     return i;
 }
 
-void mx_cd(char **args) {
-    //struct stat buf;
-    bool fl_P = false, fl_s = false;
-    errno = 0;
-
-    int res, argc = count_args(args);
-    while((res = getopt(argc, args, "Ps")) != -1) {
-        switch(res) {
-            case 'P':
-                fl_P = true;
-                printf("fl_P found\n");
-                break;
-            case 's':
-                fl_s = true;
-                printf("fl_s found\n");
-                break;
-            default:
-                break;
+static void check_flags(char **args, bool *fl_P, bool *fl_s) {
+    for(int i = 0; args[i] != NULL; i++) {
+        if(mx_strcmp(args[i], "-") != 0 && args[i][0] == '-') {
+            for(int j = 0; j < mx_strlen(args[i]); j++) {
+                if(args[i][j] == 'P') *fl_P = true;
+                else if(args[i][j] == 's') *fl_s = true;
+            }
         }
     }
+}
 
+void mx_cd(char **args) {
+    struct stat buf;
+    bool fl_P = false, fl_s = false;
+    errno = 0;
+    int argc = count_args(args);
+    check_flags(args, &fl_P, &fl_s);
     char *path = args[argc - 1]; 
-
+    lstat(path, &buf);
     if(!prev_path) { //init pathes
         prev_path = malloc(sizeof(char) * 1024);
         getcwd(prev_path, 1024);
@@ -71,9 +67,13 @@ void mx_cd(char **args) {
         return;
     }
 
-    
-
     char *fixed_path = mx_replace_substr(path, "~", getenv("HOME")); // ~ fix
+
+    if(S_ISLNK(buf.st_mode) && !fl_P) {
+        setenv("PWD", realpath(fixed_path, NULL), 1);
+    }
+
+    
 
     chdir(fixed_path); //default
     get_new_pathes(&prev_path, &cur_path);
